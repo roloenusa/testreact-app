df3d6bda37100d291e9f5009258550bc6d0f1ee3
Juan Delgado <roloenusa@msn.com>
2020-04-07 22:38:12 -0700

    Display support for speech

--- src/sentence/sentence-view.js
+++ src/sentence/sentence-view.js
@@ -1,11 +1,22 @@
+import Speech from 'speak-tts'
 import Speak from './../speak'
 import React from 'react';
 import { observer } from 'mobx-react'
 
 import Sentence from './sentence';
 
+
+function isSupported() {
+    const speech = new Speech();
+    if(speech.hasBrowserSupport()) { // returns a boolean
+        console.log("speech synthesis supported")
+        return true;
+    }
+    return false;
+}
+
 const SentenceView = observer((props) => (
-    <span>{Sentence.toString()} <button onClick={() => Speak(Sentence.toString())}>READ</button></span>
+<span>{Sentence.toString()} <button onClick={() => Speak(Sentence.toString())}>READ {isSupported() ? 'YES' : 'NO'}</button></span>
 ))
 
 export default SentenceView;
